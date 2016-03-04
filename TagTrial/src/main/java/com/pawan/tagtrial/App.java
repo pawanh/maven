a4f2a917d64a3e7b63ae5eee088ed48279aae150
hotwanpg <pawan.hotwani@gmail.com>
2016-03-04 23:51:54 +0000

    First Commit

---
+++ TagTrial/src/main/java/com/pawan/tagtrial/App.java
@@ -0,0 +1,13 @@
+package com.pawan.tagtrial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
